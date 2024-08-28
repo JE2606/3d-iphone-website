0f0006e4a2b42fe425ef73ddf876a5054b0ebc9f
JE2606 <juan.elias0626@gmail.com>
2024-08-28 12:41:53 -0400

    arreglo en deploy

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,7 +3,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
-import IPhone from './IPhone';
+import IPhone from './IPhone.jsx';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
