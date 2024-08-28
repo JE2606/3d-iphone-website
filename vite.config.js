b288bf39dbea63c2119d251179b692fee9604430
JE2606 <juan.elias0626@gmail.com>
2024-08-28 13:40:37 -0400

    prueba

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   build: {
     rollupOptions: {
       output: {
