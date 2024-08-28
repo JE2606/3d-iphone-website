a8eec116e3424666c0465fcb09e087dacbfbf8e3
JE2606 <juan.elias0626@gmail.com>
2024-08-28 12:10:28 -0400

    ADD BUILD

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true
